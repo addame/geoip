3b1f54fd1561b4175036414c698d3745f27acd22
root <root@rails-shtab.(none)>
2011-09-01 10:50:25 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoip::Application.config.secret_token = '38180ebe1fc8165593291b0eeda98614b60dfe1ea2ef37da3f1e140ee9d0b6752169ca1abc5cc2ad66045647e71ad2006e165ad86bb6cede146c1ba199694284'
