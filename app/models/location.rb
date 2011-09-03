5875850602249764fd5d29fa4eb81212e0f1d60d
Roman Franko <muk_mail@ukr.net>
2011-09-03 22:46:22 +0300

    add markers info

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,6 +5,15 @@ class Location < ActiveRecord::Base
   def gmaps4rails_address
     "#{self.address}" 
   end
+  def gmaps4rails_infowindow
+    "<h1>#{self.name}</h1>"
+  end
+  def gmaps4rails_link
+    id.to_s
+  end
+  def gmaps4rails_sidebar
+    "#{self.name}" 
+  end
   geocoded_by :ip_address  
   after_validation :geocode
 end
